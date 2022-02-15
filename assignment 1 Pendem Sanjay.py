81f61e712d461731fd7fb9b049681740d877e9e4
sanjay916 <99668976+sanjay916@users.noreply.github.com>
2022-02-15 07:37:29 +0530

    DAY 1, Assignment 1, PENDEM SANJAY

---
+++ assignment 1 Pendem Sanjay.py
@@ -0,0 +1,20 @@
+import numpy as np
+#NAME:PENDEM SANJAY
+#Question 1:
+a=np.array(range(-10,11))
+#print(a)
+#Question 2:
+D = np.array(range(18)) + 3
+#print(D)
+x=D[1:9]
+#print(x)
+y=D[-10:-2]
+#print(y)
+z=D[::-1]
+#print(z)
+#Question 3:
+'''Numpy arrays are faster than lists and take up less space.
+Lists are default feature data structures but Numpy arrays are from its library'''
+#Question 4:
+'''integer,boolean,float,double,string,datetime,object,void
+'''
